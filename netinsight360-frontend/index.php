5438b0d5483137ab340d84c502e9e31a0f88426b
Doyen5 <princedesirek@gmail.com>
2026-05-10 15:34:18 +0000

    ajout des fichiers css et js dans le dossier assets et modification des liens dans index.php

--- netinsight360-frontend/index.php
+++ netinsight360-frontend/index.php
@@ -19,9 +19,9 @@ if (AuthHelper::isLoggedIn()) {
     <link rel="icon" type="image/png" href="assets/img/logo.PNG">
     
     <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap-icons.min.css">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <!-- Custom CSS -->
